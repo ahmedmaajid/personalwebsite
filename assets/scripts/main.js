cbefd29a61fa7ee2434814aa0ba32d78cb7abe13
ahmedmaajid <119845229+ahmedmaajid@users.noreply.github.com>
2023-06-26 19:42:32 +0530

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -16,3 +16,31 @@ toggleBar.addEventListener("click", () => {
 // if (window.matchMedia("(orientation: landscape)").matches) {
 //   alert("Please turn your device to portrait mode for better look.");
 // }
+const model = document.querySelector(".box .model img");
+const box = document.querySelector(".box");
+const gallery = document.querySelector(".gallery");
+const allImgs = document.querySelectorAll(".gallery img");
+const closeButton = document.querySelector(".model p span");
+
+allImgs.forEach((img) =>
+  img.addEventListener("click", (e) => {
+    openModel(e);
+  })
+);
+
+function openModel(e) {
+  currentImgSource = e.target.src;
+  box.style.display = "block";
+  model.src = currentImgSource;
+
+  gallery.style.filter = "blur(10px)";
+  gallery.style.pointerEvents = "none";
+
+  box.scrollIntoView({ behavior: "smooth", block: "center" });
+}
+
+closeButton.addEventListener("click", () => {
+  box.style.display = "none";
+  gallery.style.filter = "unset";
+  gallery.style.pointerEvents = "unset";
+});
